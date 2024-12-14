442673d6be83d5b4309d58eef10f9c846537723f
msadlej <01178587@pw.edu.pl>
2024-12-14 20:42:19 +0100

    add backend structure

---
+++ backend/app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Response
+from app.routers import users
+import uvicorn
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return Response("Server is running.")
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
