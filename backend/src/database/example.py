aea8c637adadb2dd7eb3f884d2a4d987f1bc7aa9
msadlej <01178587@pw.edu.pl>
2024-12-15 18:03:55 +0100

    resolve conflicts

--- backend/src/database/example.py
+++ backend/src/database/example.py
@@ -1,6 +1,6 @@
-from main import MongoDB, User, Bot, GameType, Tournament, Match
+from database.main import MongoDB, User, Bot, GameType, Tournament, Match
 from datetime import datetime
-from bson import ObjectId
+
 
 if __name__ == "__main__":
     db = MongoDB()
@@ -9,39 +9,22 @@ if __name__ == "__main__":
     game_types = GameType(db)
     tournaments = Tournament(db)
     matches = Match(db)
-    
+
     chess_id = game_types.create_game_type(
-        "Chess",
-        "Classical chess game with standard rules"
-    )
-    
-    user_id1 = users.create_user(
-        "standard_user",
-        "hashed_password",
-        "standard"
+        "Chess", "Classical chess game with standard rules"
     )
 
-    user_id2 = users.create_user(
-        "premium_user",
-        "hashed_password",
-        "premium"
-    )
-    
-    bot_id1 = bots.create_bot(
-        "Chess_Bot_1",
-        chess_id,
-        "chess_bot_code_here"
-    )
+    user_id1 = users.create_user("standard_user", "hashed_password", "standard")
 
-    bot_id2 = bots.create_bot(
-        "Chess_Bot_2",
-        chess_id,
-        "chess_bot_code_here"
-    )
-    
+    user_id2 = users.create_user("premium_user", "hashed_password", "premium")
+
+    bot_id1 = bots.create_bot("Chess_Bot_1", chess_id, "chess_bot_code_here")
+
+    bot_id2 = bots.create_bot("Chess_Bot_2", chess_id, "chess_bot_code_here")
+
     users.add_bot(user_id1, bot_id1)
     users.add_bot(user_id2, bot_id2)
-    
+
     tournament_id = tournaments.create_tournament(
         "Chess Tournament",
         "Description of the tournament",
@@ -49,21 +32,20 @@ if __name__ == "__main__":
         user_id2,
         datetime.now().strftime("%Y-%m-%d %H:%M"),
         "7314",
-        16
+        16,
     )
-    
+
     tournaments.add_participant(tournament_id, bot_id1)
     tournaments.add_participant(tournament_id, bot_id2)
-    
+
     match_id = matches.create_match(1, bot_id1, bot_id2)
-    
+
     matches.add_move(match_id, "e2e4")
     matches.add_move(match_id, "e7e5")
     matches.add_move(match_id, "f2f4")
     matches.add_move(match_id, "f7f5")
 
-    
     matches.set_winner(match_id, bot_id1)
-    
+
     bots.update_stats(bot_id1, won=True)
     bots.update_stats(bot_id2, won=False)
