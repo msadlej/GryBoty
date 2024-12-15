5002419e8a5cf997a601e39007ad68dc58ac9e3e
msadlej <01178587@pw.edu.pl>
2024-12-16 00:19:28 +0100

    add tournament access checks

--- backend/src/app/routers/tournaments.py
+++ backend/src/app/routers/tournaments.py
@@ -1,5 +1,6 @@
+from app.models.tournament import get_tournaments_by_creator, get_tournament_by_id
 from app.utils.authentication import get_current_active_user
-from app.models.tournament import get_tournaments_by_creator
+from app.schemas.tournament import TournamentModel
 from fastapi import APIRouter, Depends
 from app.schemas.user import UserModel
 from typing import Annotated
@@ -12,4 +13,22 @@ router = APIRouter()
 async def read_own_tournaments(
     current_user: Annotated[UserModel, Depends(get_current_active_user)],
 ):
+    tournaments: list[TournamentModel] | None = get_tournaments_by_creator(current_user)
+
+    if tournaments is None:
+        return {"detail": "No tournaments found."}
     return get_tournaments_by_creator(current_user)
+
+
+@router.get("/tournaments/{tournament_id}")
+async def read_tournament_by_id(
+    current_user: Annotated[UserModel, Depends(get_current_active_user)],
+    tournament_id: str,
+):
+    tournament: TournamentModel | None = get_tournament_by_id(
+        current_user, tournament_id
+    )
+
+    if tournament is None:
+        return {"detail": "Tournament not found."}
+    return get_tournament_by_id(current_user, tournament_id)
