f0798cb5868aa74d5664bfa2d8990655aa60fb67
msadlej <01178587@pw.edu.pl>
2024-12-20 20:36:04 +0100

    fix mypy issues

--- backend/src/app/routers/games.py
+++ backend/src/app/routers/games.py
@@ -2,7 +2,7 @@ from app.utils.authentication import get_current_active_user
 from app.models.game import get_all_game_types
 from fastapi import APIRouter, Depends
 from app.schemas.game import GameModel
-from app.models.user import UserModel
+from app.schemas.user import UserModel
 
 
 router = APIRouter(prefix="/games")
