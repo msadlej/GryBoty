e0ea10cbcf45c79bd81761296e79465ba9d21a1f
msadlej <01178587@pw.edu.pl>
2024-12-20 20:54:20 +0100

    add match tests

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,7 +1,9 @@
 from app.utils.authentication import get_password_hash
+from app.schemas.match import MatchModel
 from app.schemas.game import GameModel
-from app.schemas.bot import BotModel
 from app.schemas.user import UserModel
+from app.schemas.bot import BotModel
+from datetime import datetime
 from bson import ObjectId
 import pytest
 
@@ -39,6 +41,33 @@ def user_dict(bot_dict):
         "account_type": "standard",
         "bots": [BotModel(**bot_dict)],
         "is_banned": True,
+    }
+
+
+@pytest.fixture
+def match_dict(bot_dict):
+    return {
+        "_id": ObjectId(),
+        "game_num": 1,
+        "players": {"player1": BotModel(**bot_dict), "player2": BotModel(**bot_dict)},
+        "moves": ["move1", "move2"],
+        "winner": BotModel(**bot_dict),
+    }
+
+
+@pytest.fixture
+def tournament_dict(game_dict, bot_dict, user_dict, match_dict):
+    return {
+        "_id": ObjectId(),
+        "name": "Test Tournament",
+        "description": "Test Description",
+        "game_type": GameModel(**game_dict),
+        "creator": UserModel(**user_dict),
+        "start_date": datetime(2024, 12, 24),
+        "access_code": "12345",
+        "max_participants": 4,
+        "participants": [BotModel(**bot_dict)],
+        "matches": [MatchModel(**match_dict)],
     }
 
 
---
+++ backend/tests/test_match.py
@@ -0,0 +1,23 @@
+from app.models.match import convert_match
+from app.schemas.match import MatchModel
+from bson import ObjectId
+
+
+def test_match_model(match_dict):
+    match = MatchModel(**match_dict)
+
+    assert ObjectId(match.id) == match_dict["_id"]
+    assert match.game_num == match_dict["game_num"]
+    assert match.players == match_dict["players"]
+    assert match.moves == match_dict["moves"]
+    assert match.winner == match_dict["winner"]
+
+
+def test_convert_match(match_dict):
+    match: MatchModel = convert_match(match_dict, detail=False)
+
+    assert ObjectId(match.id) == match_dict["_id"]
+    assert match.game_num == match_dict["game_num"]
+    assert match.players is None
+    assert match.moves is None
+    assert match.winner is None
