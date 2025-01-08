d6ba6cd3353a5b6f666f53f1a2120a74b6ecb0bb
jkosla <01178539@pw.edu.pl>
2025-01-08 13:57:50 +0100

    Just for backup, struggling with multiprocessing in Docker

--- docker/main.py
+++ docker/main.py
@@ -1,9 +1,8 @@
 from src.app.services.validation.bot_validation import BotValidationManager
 from src.app.services.run_game.bot_runner import BotRunner
 import json
-from fastapi import FastAPI, Form, HTTPException
+from fastapi import FastAPI, Form, HTTPException, UploadFile, File
 from typing import Optional
-
 
 app = FastAPI()
 
@@ -15,18 +14,19 @@ def preprocess_file(file: bytes):
 @app.post("/run-match")
 async def run_match(
     game: str = Form(...),
-    file1: bytes = Form(...),  # File content as bytes
-    file2: bytes = Form(...),  # File content as bytes
-    filename1: Optional[str] = Form(None),
-    filename2: Optional[str] = Form(None),
+    file1: UploadFile = File(...),
+    file2: UploadFile = File(...),
 ):
     try:
 
-        filename1 = filename1 or "file1.py"
-        filename2 = filename2 or "file2.py"
+        filename1 = file1.filename
+        filename2 = file2.filename  # test for the same names TODO
 
-        bot_1_str = preprocess_file(file1) # TODO think if its needed
-        bot_2_str = preprocess_file(file2)
+        # bot_1_str = preprocess_file(file1) # TODO think if its needed
+        # bot_2_str = preprocess_file(file2)
+
+        bot_1_str = file1.file.read().decode()  # TODO think if its needed
+        bot_2_str = file2.file.read().decode()
 
         runner = BotRunner(game, bot_1_str, bot_2_str, filename1, filename2)
         winner, states = runner.run_game()
@@ -35,15 +35,14 @@ async def run_match(
         return {"winner": winner, "states": result_json}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
-    
 
 
 @app.post("/validate")
-async def validate(game: str = Form(...), file: bytes = Form(...)):
+async def validate(game: str = Form(...), file: UploadFile = File(...)):
     try:
-        bot_str = preprocess_file(file)
+        bot_str = file.file.read().decode()
         validator = BotValidationManager(bot_str, game)
         validator.validate()
         return {"success": True, "message": "Validation passed."}
     except Exception as e:
-        return {"success": False, "error": str(e)}
+        raise HTTPException(status_code=400, detail=str(e))
