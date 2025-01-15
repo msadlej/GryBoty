17012c6158798e78d37fc32b5a0d76b092ae29e2
msadlej <01178587@pw.edu.pl>
2025-01-15 14:43:30 +0100

    fix: improve error handling in run_match and update bot creation with example bot code

--- backend/src/app/routers/matches.py
+++ backend/src/app/routers/matches.py
@@ -122,7 +122,7 @@ async def run_match(
 
         bot_1, bot_2 = match.players.values()
         response = conn.run_match(tournament.game_type.name, bot_1.code, bot_2.code)
-        if response["error"]:
+        if "error" in response.keys():
             raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 detail="Error running Docker commands",
