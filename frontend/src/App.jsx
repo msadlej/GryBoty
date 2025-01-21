64aad189e66c520edc3011e935077e6ee8563a6f
smakuch0 <szymon.d.makuch@gmail.com>
2025-01-21 22:49:43 +0100

    spam update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,6 @@ const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  console.log('Interceptor applied:', config);
   const token = localStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
